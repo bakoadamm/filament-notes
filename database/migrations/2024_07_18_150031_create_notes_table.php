0b4a4573346d89318c9cf20daa190ad1aa91ebd4
bakoadamm <bakoadam44@gmail.com>
2025-04-02 13:57:02 +0200

    migration team_id foreignId

--- database/migrations/2024_07_18_150031_create_notes_table.php
+++ database/migrations/2024_07_18_150031_create_notes_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
 
+            $table->foreignId('team_id');
+
             $table->string('group')->nullable()->index();
             $table->string('status')->default('pending')->nullable()->index();
 
